96f3a4dff2005aa9acc1a6a935a2cfb690d22269
Augustine Benard <pheelzconnect@gmail.com>
2022-09-19 02:01:58 +0100

    first commit

---
+++ src/test/java/com/augustine/musalasoftdroneservice/MusalaSoftDroneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustine.musalasoftdroneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusalaSoftDroneServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
